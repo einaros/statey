5067d359a8bd37e2b79559ad8204dd3436aa4fdd
einaros <einaros@gmail.com>
2011-09-03 10:21:02 +0200

    return machine api rather than states

--- statey.js
+++ statey.js
@@ -1,7 +1,8 @@
 module.exports = {
     build: function(global, states, start) {
         var stateMap = {};
-        var api = {
+        
+        var stateApi = {
             goto: function(stateName, userState) {
                 this.deactivate(userState);
                 var targetState = stateMap[stateName];
@@ -14,24 +15,29 @@ module.exports = {
                 this.deactivate(userState);
             }
         }
+        
         states.forEach(function(state) {
             stateMap[state.name] = state;
             state.global = global;
-            for (var name in api) {
-                state[name] = api[name];
+            for (var name in stateApi) {
+                state[name] = stateApi[name];
             }
         });
-        states.enter = function(stateName) {
-            var startState = stateMap[stateName];
-            if (typeof startState == 'undefined') {
-                throw 'invalid state name';
-            }
-            startState.activate.apply(startState, Array.prototype.slice.call(arguments, 1));
+        
+        var machine = {
+          enter: function(stateName) {
+              var startState = stateMap[stateName];
+              if (typeof startState == 'undefined') {
+                  throw 'invalid state name';
+              }
+              startState.activate.apply(startState, Array.prototype.slice.call(arguments, 1));
+          }
         }
+        
         if (arguments.length > 2) {
             var args = Array.prototype.slice.call(arguments, 2);
-            states.enter.apply(this, args);
+            machine.enter.apply(this, args);
         }
-        return states;
+        return machine;
     }
 }
